d40b7463bc177f201c57435a5b8016efa58e4b85
Techsultan <salamsultan09@gmail.com>
2023-08-15 00:26:11 +0100

    implemented the color picker dialog

--- app/src/main/java/com/techsultan/mynotes/fragment/NewNoteFragment.kt
+++ app/src/main/java/com/techsultan/mynotes/fragment/NewNoteFragment.kt
@@ -1,17 +1,22 @@
 package com.techsultan.mynotes.fragment
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.navigation.fragment.findNavController
 import com.github.dhaval2404.colorpicker.MaterialColorPickerDialog
 import com.github.dhaval2404.colorpicker.listener.ColorListener
 import com.github.dhaval2404.colorpicker.model.ColorSwatch
 import com.techsultan.mynotes.R
 import com.techsultan.mynotes.databinding.FragmentNewNoteBinding
+import com.thebluealliance.spectrum.internal.ColorUtil
 
 
 class NewNoteFragment : Fragment() {
@@ -38,14 +43,17 @@ class NewNoteFragment : Fragment() {
 
         binding.saveBtn.setOnClickListener {  }
 
+
         binding.colorFabBtn.setOnClickListener {
+            var mMaterialColor = ""
             MaterialColorPickerDialog
                 .Builder(requireActivity())
                 .setColorSwatch(ColorSwatch._500)
-                .setDefaultColor("")
+                .setDefaultColor(mMaterialColor)
                 .setColorListener(object : ColorListener {
                     override fun onColorSelected(color: Int, colorHex: String) {
-                        TODO("Not yet implemented")
+                        mMaterialColor = colorHex
+                        setTextFieldBackground(binding.etNote, color)
                     }
                 })
                 .setDismissListener {
@@ -53,6 +61,15 @@ class NewNoteFragment : Fragment() {
                 }
                 .show()
         }
+    }
+
+    private fun setTextFieldBackground(textField: AppCompatEditText, color: Int) {
+        if (ColorUtil.isColorDark(color)) {
+            textField.setTextColor(Color.WHITE)
+        } else {
+            textField.setTextColor(Color.BLACK)
+        }
+        textField.backgroundTintList = ColorStateList.valueOf(color)
     }
 
 
